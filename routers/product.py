dfaa29d95b3e2e27b4895166f7e19d4872a1d3f7
mark anthony dador <dador.markanthony@gmail.com>
2026-05-10 17:08:36 +0800

    put protected on route that needs

--- routers/product.py
+++ routers/product.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter,Request,Depends
+from fastapi import APIRouter,Request,Depends,HTTPException
 from ecommercedb import Product
+from verify import verify_token
 
 product_router = APIRouter(prefix='/product',tags=['product'])
 
 def product_service(request:Request):
     return Product(request.app.state.db)
 @product_router.get('/product_lists')
-def list_product(service:Product=Depends(product_service)):
+def list_product(service:Product=Depends(product_service),check_token:dict=Depends(verify_token)):
+    if not check_token:
+        raise HTTPException(status_code=401,detail='token expired or invalid')
     return service.product_list()
 
